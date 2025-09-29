482428d414ddfc967402a85777202727884d56a4
gameprogress <isakehaeev@gmail.com>
2025-09-29 14:26:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const container = document.querySelector('.carousel-container');
 
 function updatePositions() {
     const centerY = container.offsetHeight / 2;
-    items.forEach(item => {
+    items.forEach((item, index) => {
         item.style.position = 'absolute';
         item.style.width = '100%';
         item.style.left = '50%';
-        item.style.transform = `translateX(-50%) translateY(${centerY - item.offsetHeight / 2}px)`;
+        item.style.transform = `translateX(-50%) translateY(${centerY - item.offsetHeight / 2 + (index - Math.floor(items.length / 2)) * (item.offsetHeight + 20)}px)`;
     });
 }
 
